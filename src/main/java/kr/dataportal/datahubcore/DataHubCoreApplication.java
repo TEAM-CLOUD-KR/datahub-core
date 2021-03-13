a7279dd6ba2ea13f910197cf9bd89e10177bafc7
Taeyang Jin <sun@jupiterflow.com>
2021-03-13 22:32:30 +0900

    feat: SSL configure

--- src/main/java/kr/dataportal/datahubcore/DataHubCoreApplication.java
+++ src/main/java/kr/dataportal/datahubcore/DataHubCoreApplication.java
@@ -2,12 +2,20 @@ package kr.dataportal.datahubcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DataHubCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataHubCoreApplication.class, args);
+		new SpringApplicationBuilder(DataHubCoreApplication.class)
+				.properties("" +
+						"spring.config.location=" +
+						"classpath:/application.yml," +
+						"optional:C:/repository/_secrets/c4k-blog.yml," +
+						"optional:/home/datahub/_secrets/c4k-blog.yml"
+				)
+				.run(args);
 	}
 
 }
