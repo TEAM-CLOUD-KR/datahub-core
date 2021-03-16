0e5f1c6385069fa790c5b8a82e5bf0ebff6a9e07
Taeyang Jin <sun@jupiterflow.com>
2021-03-16 18:16:46 +0900

    test: add TestCode

--- src/test/java/kr/dataportal/datahubcore/DatahubCoreApplicationTests.java
+++ src/test/java/kr/dataportal/datahubcore/DatahubCoreApplicationTests.java
@@ -1,10 +1,15 @@
 package kr.dataportal.datahubcore;
 
+import kr.dataportal.datahubcore.domain.dataset.Category;
+import kr.dataportal.datahubcore.domain.dataset.DataSetGwanbo;
+import kr.dataportal.datahubcore.domain.dataset.Publish;
 import kr.dataportal.datahubcore.service.DataSetGwanboService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(properties = ("" +
         "spring.config.location=" +
@@ -22,8 +27,20 @@ class DatahubCoreApplicationTests {
     }
 
     @Test
-    void repositoryTest() {
-        Assertions.assertThat(dataSetGwanboService.findAll().size()).isGreaterThan(0);
+    @Transactional
+    @Rollback(value = true)
+    void DataSetGwanboService_저장_테스트() {
+        DataSetGwanbo gwanbo = new DataSetGwanbo(
+                "test_id",
+                new Publish("publish_id", "publish_title",
+                        "20210316", "publish_sequence", "publish_author"),
+                new Category("category_name", "category_id"),
+                "binary"
+        );
+        dataSetGwanboService.save(gwanbo);
+        DataSetGwanbo one = dataSetGwanboService.findById("test_id");
+
+        Assertions.assertThat(one.getPublish().getAuthor()).isEqualTo("publish_author");
     }
 
 }
