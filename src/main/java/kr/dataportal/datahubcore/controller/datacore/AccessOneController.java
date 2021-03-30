6151dcb89c1243b928a23684893191d903bbab4a
Taeyang Jin <sun@jupiterflow.com>
2021-03-30 10:32:05 +0900

    feat: AccessOne

---
+++ src/main/java/kr/dataportal/datahubcore/controller/datacore/AccessOneController.java
@@ -0,0 +1,21 @@
+package kr.dataportal.datahubcore.controller.datacore;
+
+import kr.dataportal.datahubcore.domain.datacore.JSONResponse;
+import kr.dataportal.datahubcore.service.DataSetCCTVService;
+import kr.dataportal.datahubcore.service.DataSetGwanboService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AccessOneController {
+    private final DataSetCCTVService dataSetCCTVService;
+    private final DataSetGwanboService dataSetGwanboService;
+
+    @GetMapping("/")
+    public JSONResponse AccessOne() {
+        return new JSONResponse(HttpStatus.OK, dataSetCCTVService.findRandomize());
+    }
+}
