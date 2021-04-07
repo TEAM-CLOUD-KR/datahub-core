5b0b93a386b1468ccc6bb1a40a04d855185b8a11
Taeyang Jin <sun@jupiterflow.com>
2021-04-07 15:31:55 +0900

    Update SecurityConfig.java

--- src/main/java/kr/dataportal/datahubcore/config/SecurityConfig.java
+++ src/main/java/kr/dataportal/datahubcore/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/dataset/**",
             "/core/**",
             "/common/util/**",
+            "/api/**",
             "/"
     };
 
