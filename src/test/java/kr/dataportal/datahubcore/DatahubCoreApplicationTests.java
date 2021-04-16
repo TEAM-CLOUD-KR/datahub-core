c31c5947983af1ba99d0ea29e2e45c93b5daaae2
Taeyang Jin <sun@jupiterflow.com>
2021-04-16 11:37:41 +0900

    refactor: static import 'assertThat'

--- src/test/java/kr/dataportal/datahubcore/DatahubCoreApplicationTests.java
+++ src/test/java/kr/dataportal/datahubcore/DatahubCoreApplicationTests.java
@@ -9,8 +9,11 @@ import kr.dataportal.datahubcore.domain.dataset.gwanbo.Category;
 import kr.dataportal.datahubcore.domain.dataset.gwanbo.DataSetGwanbo;
 import kr.dataportal.datahubcore.domain.dataset.gwanbo.Organization;
 import kr.dataportal.datahubcore.domain.dataset.gwanbo.Publish;
+import kr.dataportal.datahubcore.domain.user.SignInStatus;
+import kr.dataportal.datahubcore.domain.user.SignUpStatus;
 import kr.dataportal.datahubcore.domain.user.User;
 import kr.dataportal.datahubcore.dto.api.ApiListPagingDTO;
+import kr.dataportal.datahubcore.dto.user.UserSignInDto;
 import kr.dataportal.datahubcore.service.api.ApiListService;
 import kr.dataportal.datahubcore.service.common.Category1stService;
 import kr.dataportal.datahubcore.service.common.Category2ndService;
@@ -19,6 +22,7 @@ import kr.dataportal.datahubcore.service.dataset.cctv.DataSetCCTVService;
 import kr.dataportal.datahubcore.service.dataset.gwanbo.DataSetGwanboService;
 import kr.dataportal.datahubcore.service.user.UserService;
 import kr.dataportal.datahubcore.util.CommonUtil;
+import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +31,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(properties = ("" +
         "spring.config.location=" +
@@ -73,76 +80,95 @@ class DatahubCoreApplicationTests {
         dataSetGwanboService.save(gwanbo);
         DataSetGwanbo one = dataSetGwanboService.findBySeq("test_seq");
 
-        Assertions.assertThat(one.getPublish().getSeq()).isEqualTo("publish_seq");
+        assertThat(one.getPublish().getSeq()).isEqualTo("publish_seq");
     }
 
     @Test
     void DataSetGwanboService_랜덤조회_테스트() {
         DataSetGwanbo randomize = dataSetGwanboService.findRandomize();
-        Assertions.assertThat(randomize.getSeq()).isNotNull();
+        assertThat(randomize.getSeq()).isNotNull();
     }
 
     @Test
     void DataSetGwanbo_컬럼_전체조회_By_Class() {
         List<String> columns = CommonUtil.parseClassProperty(DataSetGwanbo.class);
-        Assertions.assertThat(columns).isNotNull();
-        Assertions.assertThat(columns.size()).isGreaterThan(0);
+        assertThat(columns).isNotNull();
+        assertThat(columns.size()).isGreaterThan(0);
     }
 
     @Test
     void DataSetCCTV_컬럼_전체조회_By_Class() {
         List<String> columns = CommonUtil.parseClassProperty(DataSetCCTV.class);
-        Assertions.assertThat(columns).isNotNull();
-        Assertions.assertThat(columns.size()).isGreaterThan(0);
+        assertThat(columns).isNotNull();
+        assertThat(columns.size()).isGreaterThan(0);
     }
 
     @Test
     void DataSetGwanbo_컬럼_전체조회_By_String() {
         List<String> columns = CommonUtil.parseClassProperty("DataSetGwanbo");
-        Assertions.assertThat(columns).isNotNull();
-        Assertions.assertThat(columns.size()).isGreaterThan(0);
+        assertThat(columns).isNotNull();
+        assertThat(columns.size()).isGreaterThan(0);
     }
 
     @Test
     void DataSetCCTV_컬럼_전체조회_By_String() {
         List<String> columns = CommonUtil.parseClassProperty("DataSetCCTV");
-        Assertions.assertThat(columns).isNotNull();
-        Assertions.assertThat(columns.size()).isGreaterThan(0);
+        assertThat(columns).isNotNull();
+        assertThat(columns.size()).isGreaterThan(0);
     }
 
     @Test
     void ApiList_단건조회() {
-        ApiList apiList = apiListService.findBySeq(4);
-        Assertions.assertThat(apiList.getSeq()).isEqualTo(4);
+        ApiList apiList = apiListService.findBySeq(14);
+        assertThat(apiList.getSeq()).isEqualTo(14);
     }
 
     @Test
     void DataSetList_findOne() {
         DataSetList dataSetList = dataSetListService.findOne("dataset_cctv");
-        Assertions.assertThat(dataSetList.getDataSet()).isEqualTo("dataset_cctv");
+        assertThat(dataSetList.getDataSet()).isEqualTo("dataset_cctv");
     }
 
     @Test
     @Transactional(readOnly = false)
     @Rollback(value = true)
     void 회원가입테스트() {
-        User user_1 = User.create("test@example.com", "pass1", "pass1", "abc");
-        Assertions.assertThat(user_1).isNotNull();
-        Assertions.assertThat(user_1.getEmail()).isEqualTo("test@example.com");
+        Optional<User> user_1 = User.create("test@example.com", "pass1", "pass1", "abc");
 
-        int signup_1 = userService.signup(user_1);
-        Assertions.assertThat(signup_1).isNotNull();
+        user_1.ifPresent(user -> {
+            assertThat(user).isNotNull();
+            assertThat(user.getEmail()).isEqualTo("test@example.com");
 
-        Assertions.assertThat(userService.findByEmail(user_1.getEmail()).getNickname()).isEqualTo(user_1.getNickname());
+            SignUpStatus signup_1 = userService.signUp(user);
+            assertThat(signup_1).isEqualTo(SignUpStatus.SUCCESS);
 
-        User user_2 = User.create("test@example.com", "pass1", "pass2", "abc");
-        Assertions.assertThat(user_2).isNull();
+            Optional<User> byEmail = userService.findByEmail(user.getEmail());
+            byEmail.ifPresent(findUser ->
+                    assertThat(findUser.getNickname()).isEqualTo(user.getNickname())
+            );
+        });
+
+        Optional<User> user_2 = User.create("test@example.com", "pass1", "pass2", "abc");
+        assertThat(user_2.isEmpty()).isTrue();
+    }
+
+    @Test
+    @Rollback(value = true)
+    @Transactional(readOnly = false)
+    void 로그인테스트() {
+        Optional<User> user_1 = User.create("test@example.com", "pass1", "pass1", "abc");
+
+        user_1.ifPresent(user -> {
+            SignUpStatus signup_1 = userService.signUp(user);
+            SignInStatus pass1 = userService.signIn(new UserSignInDto("test@example.com", "pass1"));
+            assertThat(pass1).isEqualTo(SignInStatus.SUCCESS);
+        });
     }
 
     @Test
     void 회원조회_BySeq() {
-        User bySeq = userService.findBySeq(4);
-        Assertions.assertThat(bySeq.getSeq()).isEqualTo(4);
+        User bySeq = userService.findBySeq(17).get();
+        assertThat(bySeq.getSeq()).isEqualTo(17);
     }
 
     @Test
@@ -159,9 +185,9 @@ class DatahubCoreApplicationTests {
                 category2nd.getParent(),
                 category2nd,
                 "컴퓨터정보과",
-                userService.findBySeq(4)));
+                userService.findBySeq(17).get()));
         ApiList apiList = apiListService.findByName("API 이름 테스트123");
-        Assertions.assertThat(apiList).isNotNull();
+        assertThat(apiList).isNotNull();
     }
 
     @Test
@@ -179,20 +205,20 @@ class DatahubCoreApplicationTests {
                 apiList.getCategory2nd(),
                 "기관 수정"
         );
-        Assertions.assertThat(update.getName()).isEqualTo("API 이름 수정");
+        assertThat(update.getName()).isEqualTo("API 이름 수정");
     }
 
     @Test
     void ApiList_Paging() {
         List<ApiList> byPage = apiListService.findByPage(new ApiListPagingDTO(1, 10));
-        Assertions.assertThat(byPage).isNotNull();
-        Assertions.assertThat(byPage.size()).isGreaterThan(0);
+        assertThat(byPage).isNotNull();
+        assertThat(byPage.size()).isGreaterThan(0);
     }
 
     @Test
     void ApiList_GetCount() {
         Long count = apiListService.getCount();
-        Assertions.assertThat(count).isNotNull();
-        Assertions.assertThat(count).isGreaterThan(0);
+        assertThat(count).isNotNull();
+        assertThat(count).isGreaterThan(0);
     }
 }
