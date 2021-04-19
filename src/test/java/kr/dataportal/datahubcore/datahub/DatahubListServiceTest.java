4371999f8d258412103f17ded59ab7e65eb282e1
Taeyang Jin <sun@jupiterflow.com>
2021-04-19 09:52:36 +0900

    Create DatahubListServiceTest.java

---
+++ src/test/java/kr/dataportal/datahubcore/datahub/DatahubListServiceTest.java
@@ -0,0 +1,13 @@
+package kr.dataportal.datahubcore.datahub;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = ("" +
+        "spring.config.location=" +
+        "classpath:/application.yml," +
+        "optional:C:/repository/_secrets/datahub-core.yml," +
+        "optional:/home/datahub/_secrets/datahub-core.yml," +
+        "optional:/Users/sun/repository/_secrets/datahub-core.yml"
+))
+public class DatahubListServiceTest {
+}
